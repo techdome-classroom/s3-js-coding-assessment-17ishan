ec355d57aba0f05be9b9fef8e89e288fdbb4b191
17ishan <114206488+17ishan@users.noreply.github.com>
2024-09-30 15:17:23 +0530

    Update program1.js

--- program1.js
+++ program1.js
@@ -3,8 +3,30 @@
  * @return {boolean}
  */
 var isValid = function(s) {
+    let st = [];
     
-};
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === '(' || s[i] === '{' || s[i] === '[') {
+            st.push(s[i]); 
+        } else {
+            
+            if (st.length > 0 &&
+                ((st[st.length - 1] === '(' && s[i] === ')') ||
+                 (st[st.length - 1] === '{' && s[i] === '}') ||
+                 (st[st.length - 1] === '[' && s[i] === ']'))) {
+                st.pop(); 
+            } else {
+                return false; 
+            }
+        }
+    }
+    
+   
+    return st.length === 0;
+}
+
+    
+
 
 module.exports = { isValid };
 
